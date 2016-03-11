0378f5f04379f2abae06c11a393720e7c032fd75
bracintosh <bracapellan@gmail.com>
2016-03-10 21:55:07 -0400

    More changes made to humanizer.rb, the code can be improved.

--- humanizer.rb
+++ humanizer.rb
@@ -10,47 +10,44 @@ def humanizer(int)
 
   #Get the total digits of the number
   numbers = int.to_s.split("")
-  length = numbers.length
+  digits = numbers.length
 
   #Single digit number
-  if length == 1
+  if digits == 1
     result = zero_to_29[int]
-  elsif length == 2
+  #Two digits number  
+  elsif digits == 2
 
     if int <= 29
       result = zero_to_29[int]
-    else
-      if int >=30 && int <= 100
-        result = tens_and_hundreds[int]
-      end
+    elsif int >=30 && int <= 100
+      result = tens_and_hundreds[int]
+    end
 
-      if !result
-        first_digit = false
-        for digit in numbers
-          #Checks if the actual digit is the first in the number
-          if !first_digit
-            first_digit = true
-            digit = digit+"0"
-            result = tens_and_hundreds[digit.to_i]
-          else
-            result += cont+zero_to_29[digit.to_i].downcase
-          end
+    if !result
+      first_digit = false
+      for digit in numbers
+        #Checks if the actual digit is the first in the number
+        if !first_digit
+          first_digit = true
+          digit = digit+"0"
+          result = tens_and_hundreds[digit.to_i]
+        else
+          result += cont+zero_to_29[digit.to_i].downcase
         end
       end
     end
-    #Three digits number
-  elsif length == 3
+  #Three digits number
+  elsif digits == 3
     #Take the first digit of the number and find it's noun
     #When taken out of the array, the digit is a String, so I convert it to Fixnum and multiply it by 100 to get it's hundreds
     first_digit = numbers[0]+"00"
     result = tens_and_hundreds[first_digit.to_i]
-
     #Take the last two digits of the number and use recursion to get their noun
     numbers = numbers.pop(2).join.to_i
     remaining_digits = humanizer(numbers)
     result += " "+remaining_digits.downcase
   end
-
     # Print the number's noun
     result
 end
